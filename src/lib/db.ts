f4a56e894fec59bd265a190b89b129fc351dda67
muhammed afsal <muhammedafsal078@gmail.com>
2026-03-26 23:33:08 +0530

    fixed db issue

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,40 +1,16 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
-
-let cached = (global as any).mongoose;
-
-if (!cached) {
-  cached = (global as any).mongoose = { conn: null, promise: null };
-}
-
 async function connectDB() {
-  if (cached.conn) {
-    return cached.conn;
+  const MONGODB_URI = process.env.MONGODB_URI;
+  if (!MONGODB_URI) {
+    throw new Error('Please define the MONGODB_URI environment variable inside .env');
   }
 
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cached.promise = mongoose.connect(MONGODB_URI as string, opts).then((mongoose) => {
-      return mongoose;
-    });
-  }
-  
-  try {
-    cached.conn = await cached.promise;
-  } catch (e) {
-    cached.promise = null;
-    throw e;
+  if (mongoose.connection.readyState >= 1) {
+    return mongoose;
   }
 
-  return cached.conn;
+  return mongoose.connect(MONGODB_URI);
 }
 
 export default connectDB;
